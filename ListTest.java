ae8fb29a54aa2820bc1f67b74feea3c54412378d
Kemar Meeks <59718677+meeks21@users.noreply.github.com>
2021-06-08 21:37:03 -0700

    first commit

---
+++ ListTest.java
@@ -0,0 +1,23 @@
+package Coding_Assignment;
+
+public class ListTest {
+
+    public static void main(String[] args) {
+
+
+        LinkedIntList list = new LinkedIntList();
+        processList(list);
+    }
+
+    public static void processList(LinkedIntList list) {
+        list.add(20);
+        list.add(11);
+        list.add(73);
+        list.add(111);
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        list.isSorted(list);
+
+    }
+}
